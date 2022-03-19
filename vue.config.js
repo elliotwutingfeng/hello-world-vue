6261d01b0a1d27086c09ec22664a44dde2b43848
Tingfeng <wu.tingfeng@u.nus.edu>
2022-03-19 13:51:21 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    pluginOptions: {
+        i18n: {
+            locale: "fr",
+            fallbackLocale: "en",
+            localeDir: "assets/locales",
+            enableInSFC: true,
+        },
+    },
+};
